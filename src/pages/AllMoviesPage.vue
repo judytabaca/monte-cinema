9fe8b630d644b80921bf4cb6a720b8aeab0c1604
Judyta Baca <bacajudyta@gmail.com>
2022-02-28 22:00:21 +0100

    programmtic navigation & router-view

--- src/pages/AllMoviesPage.vue
+++ src/pages/AllMoviesPage.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1>All Movies Page</h1>
+    <router-link to="/movie/1">Movie Number 1</router-link>
+    <a href="" @click="movie2">Movie Number 2</a>
   </div>
 </template>
 
 <script>
 export default {
   name: "AllMoviesPage",
+  methods: {
+    movie2() {
+      this.$router.push("/movie/2");
+    },
+  },
 };
 </script>
 

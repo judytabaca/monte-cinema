3f4fdc9eac94316bdeb3d1d745a6af83601814e6
Judyta Baca <bacajudyta@gmail.com>
2022-03-09 21:57:36 +0100

    all movies search

---
+++ src/components/UI/MainDropdown.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <label for=""></label>
+    <select></select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainDropdown",
+};
+</script>
+
+<style lang="scss" scoped></style>

8966e26181bdc04958560d57c071d363b0f44ea7
Judyta Baca <bacajudyta@gmail.com>
2022-03-14 19:13:03 +0100

    added mixins

--- src/pages/MovieDetailsPage.vue
+++ src/pages/MovieDetailsPage.vue
@@ -3,14 +3,14 @@
     <div class="movie-page__description">
       <h1>{{ movie[0].title }}</h1>
       <div>
-        <span class="movie-page__description__genre">{{
+        <span class="movie-page__description-genre">{{
           movie[0].genre.name
         }}</span>
-        <span class="movie-page__description__length">{{
+        <span class="movie-page__description-length">{{
           toHoursAndMinutes(movie[0].length)
         }}</span>
       </div>
-      <p>{{ movie[0].description }}</p>
+      <p class="movie-page__description-text">{{ movie[0].description }}</p>
     </div>
     <div class="movie-page__image">
       <img :src="movie[0].poster_url" alt="" />
@@ -45,19 +45,48 @@ export default {
 <style lang="scss" scoped>
 .movie-page {
   display: flex;
+  margin-top: 4.5em;
 
   &__description {
     width: 50%;
 
-    &__genre {
+    &-genre {
+      @include genre-button;
     }
-    &__length {
+    &-length {
+      @include time-info;
       margin-left: 15px;
     }
+    &-text {
+      font-family: "Roboto Mono";
+      font-weight: 400;
+      font-size: 22px;
+      line-height: 37px;
+      letter-spacing: 0.015em;
+      color: $tuna;
+      margin: 32px 0px;
+    }
   }
-
   &__image {
-    width: 50%;
+    width: 621px;
+    height: 289px;
+    aspect-ratio: 16/9;
+
+    img {
+      max-width: 100%;
+      min-width: 100%;
+      object-fit: cover;
+      height: 100%;
+    }
+  }
+  h1 {
+    font-family: "Eczar";
+    font-weight: 600;
+    font-size: 80px;
+    line-height: 82px;
+    letter-spacing: -0.01em;
+    color: $tuna;
+    margin: 32px 0px;
   }
 }
 </style>

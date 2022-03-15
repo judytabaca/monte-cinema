c0eafaa6e74ede8c3a78f70fa3f21d7712b2a535
Judyta Baca <bacajudyta@gmail.com>
2022-03-15 19:28:53 +0100

    seance list logic

--- src/pages/ScreeningsPage.vue
+++ src/pages/ScreeningsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Screenings</h1>
+    <h2>{{ selectedDay }}</h2>
     <div>
       <button
         class="day-tabs"
@@ -15,23 +16,23 @@
         <img src="@/assets/img/calendaricon.svg" alt="calendar-icon" />
       </button>
       <hr />
-      <div
-        v-for="movie in this.$store.getters.movieList"
-        :key="`movie_${movie.id}`"
-      >
-        <div>
-          {{ movie.title }}
-          <p
-            v-for="seance in seancesList
-              .filter(
-                (seance) => seance.datetime.substring(0, 10) === selectedDay
-              )
-              .filter((seance) => seance.movie == movie.id)"
-            :key="`seance_${seance.id}`"
-          >
-            {{ seance.datetime.substring(11, 16) }}
-          </p>
-        </div>
+      <div v-for="movieId in moviesOnTheDay" :key="`movie_${movieId}`">
+        <p>
+          {{
+            $store.getters.movieList.filter((movie) => movie.id == movieId)[0]
+              .title
+          }}
+        </p>
+        <p
+          v-for="seance in seancesList
+            .filter(
+              (seance) => seance.datetime.substring(0, 10) === selectedDay
+            )
+            .filter((seance) => seance.movie == movieId)"
+          :key="`seance_${seance.id}`"
+        >
+          {{ seance.datetime.substring(11, 16) }}
+        </p>
       </div>
     </div>
   </div>
@@ -45,14 +46,19 @@ export default {
   data() {
     return {
       daysList: [],
-      selectedDay: "2022-03-14",
+      selectedDay: "",
       seancesList: [],
       seanceOnTheDay: [],
+      moviesOnTheDay: [],
     };
   },
   computed: {
     todayDate() {
       return new Date();
+    },
+    todayDaySelection() {
+      let date = new Date();
+      return date.toJSON().substring(0, 10);
     },
   },
   methods: {
@@ -79,25 +85,39 @@ export default {
         .getAttribute("data-date")
         .substring(0, 10);
       console.log(this.selectedDay);
+      this.setMoviesOnTheDay();
+    },
+    setMoviesOnTheDay() {
+      const seancesOnTheDay = this.seancesList.filter(
+        (seance) => seance.datetime.substring(0, 10) === this.selectedDay
+      );
+      const uniqueMovies = [
+        ...new Set(seancesOnTheDay.map((seance) => seance.movie)),
+      ];
+      this.moviesOnTheDay = uniqueMovies;
     },
     async getSeancesList() {
       this.seancesList = await apiSeancesService.getSeancesList();
+      this.setMoviesOnTheDay();
+    },
+    makeDayList() {
+      this.daysList.push({ id: 1, date: new Date(), name: "Today" });
+      for (let i = 1; i < 6; i++) {
+        const date = new Date(
+          this.todayDate.setDate(this.todayDate.getDate() + 1)
+        );
+
+        this.daysList.push({
+          id: i + 1,
+          date: date,
+          name: date.getDay(),
+        });
+      }
     },
   },
   mounted() {
-    this.daysList.push({ id: 1, date: new Date(), name: "Today" });
-    for (let i = 1; i < 5; i++) {
-      const date = new Date(
-        this.todayDate.setDate(this.todayDate.getDate() + 1)
-      );
-
-      this.daysList.push({
-        id: i + 1,
-        date: date,
-        name: date.getDay(),
-      });
-    }
-
+    this.selectedDay = this.todayDaySelection;
+    this.makeDayList();
     this.getSeancesList();
   },
 };

469f1c2bdf5db722deb928886da5bc508629fccf
Judyta Baca <baca.judyta@gmail.com>
2022-03-17 12:36:44 +0100

    breadcrumbs component

--- src/pages/MovieDetailsPage.vue
+++ src/pages/MovieDetailsPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <BreadCrumbs :secondLevel="movieId">Movies</BreadCrumbs>
     <h1>Movie Details</h1>
     <h3>Movie ID: {{ movieId }}</h3>
   </div>
 </template>
 
 <script>
+import BreadCrumbs from "@/components/UI/BreadCrumbs.vue";
+
 export default {
   name: "MovieDetailsPage",
   props: {
@@ -13,6 +16,9 @@ export default {
       type: String,
       required: true,
     },
+  },
+  components: {
+    BreadCrumbs,
   },
 };
 </script>

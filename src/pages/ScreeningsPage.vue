f589d42bbaaee36bd8e5a1ccd8c004db32a408ff
Judyta Baca <bacajudyta@gmail.com>
2022-03-18 15:00:42 +0100

    pr revisions

--- src/pages/ScreeningsPage.vue
+++ src/pages/ScreeningsPage.vue
@@ -7,10 +7,9 @@
         class="day-tabs"
         v-for="day in daysList"
         :key="day.id"
-        :data-date="day.date.toJSON()"
-        @click="setSelectedDay"
+        @click="setSelectedDay(day.date)"
       >
-        {{ day.id === 1 ? day.name : niceDay(day.name) }}
+        {{ day.name }}
       </button>
       <button class="day-tabs__calendar">
         <img src="@/assets/img/calendaricon.svg" alt="calendar-icon" />
@@ -24,11 +23,7 @@
           }}
         </p>
         <p
-          v-for="seance in seancesList
-            .filter(
-              (seance) => seance.datetime.substring(0, 10) === selectedDay
-            )
-            .filter((seance) => seance.movie == movieId)"
+          v-for="seance in seancesByMovie(movieId)"
           :key="`seance_${seance.id}`"
         >
           {{ seance.datetime.substring(11, 16) }}
@@ -45,11 +40,8 @@ export default {
   name: "ScreeningsPage",
   data() {
     return {
-      daysList: [],
       selectedDay: "",
       seancesList: [],
-      seanceOnTheDay: [],
-      moviesOnTheDay: [],
     };
   },
   computed: {
@@ -58,67 +50,54 @@ export default {
     },
     todayDaySelection() {
       let date = new Date();
-      return date.toJSON().substring(0, 10);
+      return date.toISOString().substring(0, 10);
     },
-  },
-  methods: {
-    niceDay(dayValue) {
-      switch (dayValue) {
-        case 1:
-          return "Mon";
-        case 2:
-          return "Tue";
-        case 3:
-          return "Wed";
-        case 4:
-          return "Thu";
-        case 5:
-          return "Fri";
-        case 6:
-          return "Sat";
-        case 0:
-          return "Sun";
-      }
-    },
-    setSelectedDay(event) {
-      this.selectedDay = event.currentTarget
-        .getAttribute("data-date")
-        .substring(0, 10);
-      console.log(this.selectedDay);
-      this.setMoviesOnTheDay();
-    },
-    setMoviesOnTheDay() {
-      const seancesOnTheDay = this.seancesList.filter(
-        (seance) => seance.datetime.substring(0, 10) === this.selectedDay
-      );
-      const uniqueMovies = [
-        ...new Set(seancesOnTheDay.map((seance) => seance.movie)),
-      ];
-      this.moviesOnTheDay = uniqueMovies;
-    },
-    async getSeancesList() {
-      this.seancesList = await apiSeancesService.getSeancesList();
-      this.setMoviesOnTheDay();
-    },
-    makeDayList() {
-      this.daysList.push({ id: 1, date: new Date(), name: "Today" });
+    daysList() {
+      let daysList = [];
+      daysList.push({ id: 1, date: new Date(), name: "Today" });
       for (let i = 1; i < 6; i++) {
         const date = new Date(
           this.todayDate.setDate(this.todayDate.getDate() + 1)
         );
 
-        this.daysList.push({
+        daysList.push({
           id: i + 1,
           date: date,
-          name: date.getDay(),
+          name: date.toLocaleDateString("en-US", { weekday: "short" }),
         });
       }
+      return daysList;
+    },
+    moviesOnTheDay() {
+      return [...new Set(this.seancesOnTheDay.map((seance) => seance.movie))];
+    },
+    seancesOnTheDay() {
+      return this.seancesList.filter(
+        (seance) => seance.datetime.substring(0, 10) === this.selectedDay
+      );
+    },
+  },
+  methods: {
+    setSelectedDay(date) {
+      this.selectedDay = date.toISOString().substring(0, 10);
+    },
+    async getSeancesList() {
+      const fullList = await apiSeancesService.getSeancesList();
+      this.seancesList = fullList.filter(
+        (seance) => seance.datetime.substring(0, 10) === this.selectedDay
+      );
+    },
+    seancesByMovie(movieId) {
+      return this.seancesOnTheDay.filter((seance) => seance.movie == movieId);
+    },
+  },
+  watch: {
+    selectedDay() {
+      this.getSeancesList();
     },
   },
   mounted() {
     this.selectedDay = this.todayDaySelection;
-    this.makeDayList();
-    this.getSeancesList();
   },
 };
 </script>

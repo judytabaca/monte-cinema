4bdea3add5515e9ea7e99900735055fa5e781a1a
Judyta Baca <bacajudyta@gmail.com>
2022-03-27 19:10:52 +0200

    post install fixes

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,23 @@ import apiMoviesService from "../services/api/apiMoviesService";
 
 Vue.use(Vuex);
 
+export interface Movie {
+  id: number;
+  title: string;
+  genre: {
+    id: number;
+    name: string;
+  };
+  poster_url: string;
+  length: number;
+  release_date: string;
+  description: string;
+}
+
 export default new Vuex.Store({
   state: {
-    moviesRecord: [],
-    genreList: [],
+    moviesRecord: [] as Array<Movie>,
+    genreList: [] as Array<string>,
     selectedGenre: "",
   },
   getters: {
@@ -46,7 +59,7 @@ export default new Vuex.Store({
   actions: {
     async getMovies({ commit }) {
       try {
-        const movies = await apiMoviesService.getMovieList();
+        const movies: Array<Movie> = await apiMoviesService.getMovieList();
         commit("setMovies", movies);
       } catch (err) {
         console.log(err);
@@ -54,7 +67,7 @@ export default new Vuex.Store({
     },
     async getGenreList({ commit }) {
       try {
-        const movieList = await apiMoviesService.getMovieList();
+        const movieList: Array<Movie> = await apiMoviesService.getMovieList();
         const uniqueGenres = [
           ...new Set(movieList.map((movie) => movie.genre.name)),
         ];

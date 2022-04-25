3252448a94227e6c65954e2c70d3e9e3e572aaf5
Judyta Baca <bacajudyta@gmail.com>
2022-04-25 08:21:45 +0200

    coputed clean-up

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -41,19 +41,60 @@
             <MainButton button-type="primary">Login</MainButton>
           </router-link>
         </template>
-        <!-- TODO: mobile menu -->
-        <!-- <div class="menu__toggle">
-          <div>
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-          </div>
-        </div> -->
       </div>
     </template>
     <template v-if="isLogin || isRegister">
       <div class="with-love">designed with ❤️ by monterail</div>
     </template>
+    <div class="menu__toggle" @click="mobileMenuOn = !mobileMenuOn">
+      <div :class="buttonActive">
+        <div class="line line1"></div>
+        <div class="line line2"></div>
+        <div class="line line3"></div>
+      </div>
+    </div>
+    <Transition>
+      <div class="mobile-menu" v-if="mobileMenuOn">
+        <div class="mobile-menu__buttons">
+          <template v-if="isLoggedIn">
+            <MainButton button-type="primary" @click="logout"
+              >Logout</MainButton
+            >
+          </template>
+          <template v-else>
+            <router-link :to="{ name: 'RegisterPage' }">
+              <MainButton button-type="secondary">Register</MainButton>
+            </router-link>
+            <router-link :to="{ name: 'LoginPage' }">
+              <MainButton button-type="primary">Login</MainButton>
+            </router-link>
+          </template>
+        </div>
+        <ul class="mobile-menu__links">
+          <li>
+            <router-link
+              :to="{ name: 'AllMovies' }"
+              class="header__navbar-options"
+              >Movies</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'ScreeningsPage' }"
+              class="header__navbar-options"
+              >Screenings</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'ContactPage' }"
+              class="header__navbar-options"
+              >Contact us</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </Transition>
   </header>
 </template>
 
@@ -62,6 +103,11 @@ import MainButton from "./UI/MainButton.vue";
 
 export default {
   name: "TheHeader",
+  data() {
+    return {
+      mobileMenuOn: false,
+    };
+  },
   computed: {
     isLogin() {
       return this.$route.name === "LoginPage";
@@ -71,6 +117,20 @@ export default {
     },
     isLoggedIn() {
       return this.$store.getters.isLoggedIn;
+    },
+    line1active() {
+      return { "line1--active": this.mobileMenuOn };
+    },
+    line2active() {
+      return { "line2--active": this.mobileMenuOn };
+    },
+    line3active() {
+      return { "line3--active": this.mobileMenuOn };
+    },
+    buttonActive() {
+      return this.mobileMenuOn
+        ? "menu__toggle__lines--active"
+        : "menu__toggle__lines";
     },
   },
   methods: {
@@ -85,12 +145,64 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
 .header {
   width: 100%;
   height: 100px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative;
+
+  .mobile-menu {
+    background-color: white;
+    width: 100%;
+    height: 170px;
+    position: fixed;
+    top: 80px;
+    right: 0;
+    z-index: 99;
+    box-shadow: 0px 30px 24px 0px rgba(66, 68, 90, 0.32);
+
+    &__buttons {
+      width: 100%;
+      height: 80px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 40px;
+    }
+
+    &__links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 80px;
+
+      li {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+
+        &:hover,
+        &:active {
+          font-weight: 700;
+          color: $gray-abbey;
+          box-shadow: inset 0px -4px 0px $cherry-red;
+        }
+      }
+    }
+  }
 
   .with-love {
     font-weight: 500;
@@ -136,36 +248,78 @@ export default {
     .button {
       margin-left: 1em;
     }
+  }
 
-    .menu__toggle {
-      height: 100%;
-      width: 60px;
-      cursor: pointer;
-      display: none;
-      justify-content: center;
-      align-self: center;
+  .menu__toggle {
+    height: 100%;
+    width: 60px;
+    cursor: pointer;
+    display: none;
+    justify-content: center;
+    align-items: center;
 
+    &__lines {
       .line {
         width: 32px;
         height: 3px;
         background-color: $tuna;
         margin-bottom: 5px;
       }
+
+      .line1 {
+        transform-origin: 0% 0%;
+        transition: transform 0.4s ease-in-out;
+      }
+
+      .line2 {
+        transition: transform 0.2s ease-in-out;
+      }
+
+      .line3 {
+        transform-origin: 0% 100%;
+        transition: transform 0.4s ease-in-out;
+      }
+    }
+    &__lines--active {
+      .line {
+        width: 32px;
+        height: 3px;
+        background-color: $tuna;
+        margin-bottom: 5px;
+      }
+
+      .line1 {
+        transform: rotate(45deg) translateY(11px);
+      }
+      .line2 {
+        transform: scaleY(0);
+      }
+      .line3 {
+        transform: rotate(-45deg) translateY(-11px);
+      }
     }
   }
 }
 
-@media only screen and (max-width: 450px) {
+@media only screen and (max-width: 900px) {
   .header {
     padding: 15px 25px;
-  }
 
-  .menu__toggle {
-    display: flex;
-  }
+    &__navbar {
+      display: none;
+    }
 
-  .header__buttons .button {
-    display: none;
+    .menu__toggle {
+      display: flex;
+    }
+
+    &__buttons {
+      display: none;
+    }
+
+    .with-love {
+      display: none;
+    }
   }
 }
 </style>
